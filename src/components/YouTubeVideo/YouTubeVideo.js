d44b424b7568b4f2bbb90cf49d9f7bd6630a7a27
James Ives <iam@jamesiv.es>
2020-04-26 12:07:37 -0400

    Update YouTubeVideo.js

--- src/components/YouTubeVideo/YouTubeVideo.js
+++ src/components/YouTubeVideo/YouTubeVideo.js
@@ -2,6 +2,11 @@ import React from 'react'
 import {format as timeSince} from 'timeago.js'
 import './YouTubeVideo.scss'
 
+function htmlDecode(input) {
+  var doc = new DOMParser().parseFromString(input, 'text/html')
+  return doc.documentElement.textContent
+}
+
 const YouTubeVideo = ({title, id, thumbnail, published}) => (
   <article className="youtube-video">
     <div className="row justify-content-center align-items-center">
@@ -10,10 +15,15 @@ const YouTubeVideo = ({title, id, thumbnail, published}) => (
       </div>
 
       <div className="extra-small-8">
-        <h3 className="youtube-video--header">{title}</h3>
+        <h3 className="youtube-video--header">{htmlDecode(title)}</h3>
 
         <div className="youtube-video__meta display-flex align-items-center">
-          <a className="youtube-video__meta--link" href="youtube-video__meta--link">View on YouTube</a>
+          <a
+            className="youtube-video__meta--link"
+            href="youtube-video__meta--link"
+          >
+            View on YouTube
+          </a>
           <span className="youtube-video__meta--spacer">|</span>
           <time className="youtube-video__meta--time" dateTime={published}>
             {timeSince(published)}

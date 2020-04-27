ca572e013a00d153dd795258d5446b0090757959
James Ives <iam@jamesiv.es>
2020-04-26 22:00:01 -0400

    Meta Info

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,13 +49,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `DJ Zwackery`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#31394a`,
+        theme_color: `#849db5`,
         display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
   ],

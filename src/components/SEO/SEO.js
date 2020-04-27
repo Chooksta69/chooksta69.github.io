857015857c856abde54964e8a78d5622da402c64
James Ives <iam@jamesiv.es>
2020-04-26 21:23:57 -0400

    Update SEO.js

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -2,7 +2,7 @@
 import {graphql, useStaticQuery} from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
-import Helmet from 'react-helmet'
+import {Helmet} from 'react-helmet'
 
 export const getSchemaOrgJSONLD = ({
   datePublished,

de75496bbc7bcac3de0cb4fd94350ca599339871
James Ives <iam@jamesiv.es>
2020-05-03 00:36:02 -0400

    Footer Icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import EmailIcon from '@components/EmailIcon/EmailIcon'
+import Jives from '@images/jives.png'
 import './Footer.scss'
 
 // Renders the global footer.
@@ -9,28 +10,31 @@ const Footer = ({email}) => (
     <div className="footer__inner card background-grey">
       <div className="row align-items-center">
         <div className="extra-small-6">
-          <div className="text-left">
-            © {new Date().getFullYear()} DJ Zwackery
-            <span className="footer__inner--attribution">
-              Site created by{' '}
-              <a title="James Ives" href="https://jamesiv.es/">
-                James Ives
-              </a>
-              . Icons provided by Twitter.
-            </span>
-          </div>
-        </div>
-
-        <div className="small-6 extra-small-6">
           <div className="footer__inner--contact">
             <a
-              className="display-flex align-items-center justify-content-end"
+              className="display-flex align-items-center"
               title="Email for Contact/Bookings"
               href={`mailto:${email}`}
             >
               <EmailIcon height="30px" width="30px" />
               <span>Contact / Bookings</span>
             </a>
+          </div>
+        </div>
+        <div className="extra-small-6">
+          <div className="text-right">
+            © {new Date().getFullYear()} DJ Zwackery
+            <span className="footer__inner--attribution">
+              Icons provided by Twitter. Site created by{' '}
+              <a
+                className="footer__inner--credits"
+                title="James Ives"
+                href="https://jamesiv.es/"
+              >
+                James Ives.
+              </a>{' '}
+              <img alt="" src={Jives} />
+            </span>
           </div>
         </div>
       </div>
